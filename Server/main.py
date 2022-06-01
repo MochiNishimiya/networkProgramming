4290c4cc8e3ba0dfb794bd46f03a95a35320f340
MochiNishimiya <nguyenguyen753@gmail.com>
2022-06-01 09:06:34 +0700

    final push

--- Server/main.py
+++ Server/main.py
@@ -3,6 +3,7 @@ import hashlib
 import threading
 import random
 import time
+import csv
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 print("Socket successfully created")
@@ -10,18 +11,21 @@ port = 34214
 s.bind(('', port))
 print("socket binded to %s" % (port))
 s.listen(5)
-print ("socket is listening")
+print("socket is listening")
 server, addr = s.accept()
 print('Got connection from', addr)
 global ipList
 global vis
 global sList
 global keyList
+global idVal
 ipList = []
 idRoomList = []
 sList = []
 keyList = []
-vis = [0] * 1000    # room limited
+vis = [0] * 1000  # room limited
+idVal = 0
+
 
 def xor(arr, key):
     ans = b''
@@ -29,9 +33,11 @@ def xor(arr, key):
         ans += bytes([arr[i] ^ key[i % len(key)]])
     return ans
 
+
 def run(server, addr):
     global ipList
     global vis
+    global idVal
     roomId = -1
     bitmap = b''
 
@@ -60,15 +66,24 @@ def run(server, addr):
                 username = message.split(b'\n')[1].decode()
                 password = message.split(b'\n')[2].decode()
                 hashPass = hashlib.sha256(password.encode()).hexdigest()
-                authentication = open('log.txt', 'r').readlines()  # check authentication
+                authentication = open('log.csv')  # check authentication
+                csv_reader = csv.reader(authentication, delimiter=',')
 
                 stat = 0
-                for i, c in enumerate(authentication):
-                    checkUser, checkPass = c.split(' ')
+                for i, c in enumerate(csv_reader):
+                    if len(c) == 0: continue
+
+                    checkUser = c[0]
+                    checkPass = c[1]
+
+                    print(checkPass, checkUser)
+
                     if (checkUser.strip() == username and checkPass.strip() == hashPass):
                         server.send(xor(b'True', key))
                         stat = 1
                         break
+
+                authentication.close()
 
                 if stat == 0:
                     server.send(xor(b'False', key))
@@ -77,23 +92,26 @@ def run(server, addr):
                 username = message.split(b'\n')[1].decode()
                 password = message.split(b'\n')[2].decode()
                 hashPass = hashlib.sha256(password.encode()).hexdigest()
-                authentication = open('log.txt', 'r').readlines()  # check authentication
-                if len(authentication) == 0:
-                    log = username + ' ' + hashPass
-                else:
-                    log = '\n' + username + ' ' + hashPass
+                authentication = open('log.csv')  # check authentication
+                csv_reader = csv.reader(authentication, delimiter=',')
 
                 stat = 1
-                for i, c in enumerate(authentication):
-                    checkUser, checkPass = c.split(' ')
+                for i, c in enumerate(csv_reader):
+                    if len(c) == 0: continue
+                    checkUser, checkPass = c[0], c[1]
                     if (checkUser == username):
                         server.send(xor(b'False', key))
                         stat = 0
                         break
+                authentication.close()
 
                 if stat == 1:
-                    file = open('log.txt', 'a')
-                    file.write(log)
+                    file = open('log.csv', 'a', newline='')
+                    k = []
+                    k.append(username)
+                    k.append(hashPass)
+                    csvwriter = csv.writer(file)
+                    csvwriter.writerow(k)
                     file.close()
                     server.send(xor(b'True', key))
 
@@ -106,10 +124,8 @@ def run(server, addr):
                         break
 
                 if stat == 0:
-                    id = 0
-                    while (vis[id] == 1): id += 1
-                    vis[id] = 1
-                    roomId = id
+                    roomId = idVal
+                    idVal += 1
                     ipList.append(addr)
                     idRoomList.append(id)
                     sList.append(server)
@@ -131,7 +147,7 @@ def run(server, addr):
                         print(chat)
                         sList[i].send(xor(chat, keyList[i]))
 
-            elif message[:4] == b'\x89PNG':   # Drawing
+            elif message[:4] == b'\x89PNG':  # Drawing
                 bitmap = message
                 print(message)
                 print(addr)
